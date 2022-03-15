8bf0a0f3de2b166cafe45f034c1b1ced4b3602e7
Aintripin <yelisey_toyota@mail.ru>
2022-03-15 23:29:45 +0300

    added formatting to numbers

--- calculator_v00/src/App.js
+++ calculator_v00/src/App.js
@@ -106,6 +106,19 @@ function reducer(state, {type, payload}) {
   }
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+
+})
+
+function formatOperand(operand) {
+  if (operand == null) return     // returns nothing
+  const [integer, decimal] = operand.split('.')
+  if (decimal == null) {
+    return INTEGER_FORMATTER.format(integer)
+  }
+}
+
 function evaluate({currentOperand, previousOperand, operation}) {
   const prev = parseFloat(previousOperand)
   const current = parseFloat(currentOperand)
@@ -137,8 +150,8 @@ function App() {
   return (
     <div className = "calculator-grid">
       <div className = "output">
-        <div className = "previous-operand"> {previousOperand} {operation} </div>
-        <div className = "current-operand"> {currentOperand} </div>
+        <div className = "previous-operand"> {formatOperand(previousOperand)} {operation} </div>
+        <div className = "current-operand"> {formatOperand(currentOperand)} </div>
       </div>
 
 
